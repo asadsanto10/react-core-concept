7d451bfcbb7eba7ba3fde901c454605e88b1042f
asadsanto10 <santo.meridian@gmail.com>
2020-08-22 13:09:26 +0600

    Updates

---
+++ precache-manifest.20fc1789518495bc5e41aa65d7b41d44.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c74ac944a8236638b5e596c7476bf76a",
+    "url": "/react-core-concept/index.html"
+  },
+  {
+    "revision": "561594c910afc4c952e8",
+    "url": "/react-core-concept/static/css/main.efcaf589.chunk.css"
+  },
+  {
+    "revision": "3414bfe3e53ff53f7e29",
+    "url": "/react-core-concept/static/js/2.28f0e96d.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/react-core-concept/static/js/2.28f0e96d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "561594c910afc4c952e8",
+    "url": "/react-core-concept/static/js/main.7196c675.chunk.js"
+  },
+  {
+    "revision": "ced0a149ea02b117563c",
+    "url": "/react-core-concept/static/js/runtime-main.f0165c02.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/react-core-concept/static/media/logo.5d5d9eef.svg"
+  }
+]);
